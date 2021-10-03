4acb7a3ee98a3ea63952cc2766745365da1185b6
AmyLDunn <ldunn084@uottawa.ca>
2021-10-03 07:36:43 -0400

    Added maps functionality

--- app/src/main/java/com/example/lab02/MainActivity.java
+++ app/src/main/java/com/example/lab02/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.lab02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button maps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        maps = findViewById(R.id.button);
+
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OnOpenInGoogleMaps(view);
+            }
+        });
+    }
+
+    public void OnOpenInGoogleMaps(View view) {
+        EditText teamAddress = (EditText) findViewById(R.id.teamAddressTextView);
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+teamAddress.getText());
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
