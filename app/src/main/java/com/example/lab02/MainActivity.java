d986490452a64e4dab132862abf71555c36294ae
AmyLDunn <ldunn084@uottawa.ca>
2021-10-03 07:45:09 -0400

    Completed the activity_main.xml and MainActivity.java.

--- app/src/main/java/com/example/lab02/MainActivity.java
+++ app/src/main/java/com/example/lab02/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.lab02;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button maps;
+
+    ImageView icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
                 OnOpenInGoogleMaps(view);
             }
         });
+
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OnSetAvatarButton(view);
+            }
+        });
+
     }
 
     public void OnOpenInGoogleMaps(View view) {
@@ -34,5 +50,22 @@ public class MainActivity extends AppCompatActivity {
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         startActivity(mapIntent);
+    }
+
+    ActivityResultLauncher<Intent> profileActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                    }
+                }
+            }
+    );
+
+    public void OnSetAvatarButton(View view){
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        profileActivityResultLauncher.launch(intent);
     }
 }
